6582915cf5801ef7e7d2105a4f90194bda79ace7
18-1855-1 <annamagdalena.stranska@unicorncollege.cz>
2021-04-11 22:25:44 +0200

    Skoro hotovy projekt

--- src/main/java/cz/czechitas/java2webapps/ukol2/controler/CitControler.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controler/CitControler.java
@@ -3,7 +3,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.List;
+
 
 @Controller
 public class CitControler {
@@ -16,10 +19,22 @@ public class CitControler {
     @GetMapping("/")
     public ModelAndView vyberCit() {
         int nahodneCislo = random.nextInt(6)+1;
+        List<String> seznamTextu= Arrays.asList("Debugging /de·bugh·ing/ (verb): The Classic Mystery Game where you are the detective, the victim, and the murderer."
+                ,"A user interface is like a joke. If you have to explain it, it's not that good.",
+                "To replace programmers with robots, clients will have to accurately describe what they want. We're safe.",
+                "I have a joke on programming but it only works on my computer.",
+                "Programmer (noun.): A machine that turns coffee into code.",
+                "Real programmers count from 0.");
         ModelAndView result = new ModelAndView("t");
-        result.addObject("cislo",nahodneCislo);
+        String nahodnyCitat = seznamTextu.get(nahodneCislo-1);
+        result.addObject("citat",nahodnyCitat);
         result.addObject("obrazek",String.format("/images/obr-%d.jpg", nahodneCislo));
+        String cesta = String.format("/images/obr-%d.jpg", nahodneCislo);
+        String mujBodyStyle = "background-image: url("+cesta+");";
+        result.addObject("bodyStyle",mujBodyStyle);
+
         return result;
 
     }
+
 }
